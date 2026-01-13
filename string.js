b49a40ec3b7aa2ee141e60dafc86ab73bb8901e4
Priya chaudhary <priya-ch-0909>
2026-01-13 15:52:49 +0530

    this is my second commit

--- string.js
+++ string.js
@@ -1,3 +1,22 @@
-let sentence ="java is fun";
-let reversedSentence= sentence.split(" ").reverse().join(" ");
-console.log(reversedSentence);
+// let sentence ="java is fun";
+// let reversedSentence= sentence.split(" ").reverse().join(" ");
+// console.log(reversedSentence);
+
+// function isRotation(s1,s2){
+//     if
+// }
+
+function isRotation(s1,s2){
+    if(s1.length!==s2.length){
+        return false;
+    }
+    return (s1+s1).includes(s2);
+}
+let s1 ="abcd";
+let s2="cdab";
+if(isRotation(s1,s2)){
+    console.log("Rotation");
+}else{
+    console.log("Not a rotation")
+}
+
